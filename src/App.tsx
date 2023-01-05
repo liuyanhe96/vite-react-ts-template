581379456215c1f296ff5a830018d83ae8f17642
yanhe <yanhe.liu96@gmail.com>
2023-01-05 16:19:51 +0800

    fix(config): test cz-customizable/ 修改version

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import HelloWorld from "@/components/HelloWorld/HelloWorld";
 import styles from "./App.module.css";
 
 export default function App() {
+    console.log('test for cz-customizable')
   return (
     <main className={styles.main}>
       <img className={styles.logo} alt="React logo" width="400px" src={Logo} />
